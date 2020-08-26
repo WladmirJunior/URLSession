af5c5e6d24b0e8b6a95f9ea966d3d96547006992
Wladmir <wlad.w.junior@gmail.com>
2020-08-26 16:48:07 -0300

    Initial Commit

---
+++ URLSession3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSession3
+//
+//  Created by Wladmir  on 26/08/20.
+//  Copyright © 2020 Wladmir . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
