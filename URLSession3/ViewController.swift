4c44acf8dc8b3fd6098d9c7905200203c80f0124
Wladmir <wlad.w.junior@gmail.com>
2020-09-02 17:39:19 -0300

    feature: up project

--- URLSession3/ViewController.swift
+++ URLSession3/ViewController.swift
@@ -9,12 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        donwload()
+    }
+    
+    
+    
+    @IBAction func reload(_ sender: Any) {
+        donwload()
+    }
+    
+    private func donwload() {
+        loading.startAnimating()
+        button.setTitle("", for: .normal)
+        
+        let stringURL = "https://dog.ceo/api/breeds/image/random"
+        if let url = URL(string: stringURL) {
+            
+            let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+                
+                
+                if let data = data {
+                    
+                    do {
+                        let dogObjeto = try JSONDecoder().decode(DogApi.self, from: data)
+                        print(dogObjeto)
+                        print(dogObjeto.message)
+                        print(dogObjeto.status)
+                    } catch {
+                        print("Erro de parse")
+                    }
+                    
+//                    do {
+//                        let dataTojson = try JSONSerialization.jsonObject(
+//                            with: data,
+//                            options: .mutableContainers
+//                        ) as? [String: String]
+//
+//                        if let dicionario = dataTojson, let imagemString = dicionario["message"] {
+//                            self.downloadImage(with: imagemString)
+//                        }
+//                    } catch {
+//                        print("Erro de conversao de dados")
+//                    }
+                }
+            }
+            dataTask.resume()
+        }
     }
 
+    private func downloadImage(with link: String) {
+        if let url = URL(string: link) {
+            let dataTask = URLSession.shared
+                .dataTask(with: url) { (data, response, error) in
+                if let data = data, let imagem = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.imageView.image = imagem
+                        self.loading.stopAnimating()
+                        self.button.setTitle("Recarregar", for: .normal)
+                    }
+                }
+            }
+            dataTask.resume()
+        }
+    }
 
 }
 
+struct DogApi: Codable {
+    let message: String
+    let status: String
+}
